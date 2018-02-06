0943a7690d0881d771c31d17130861ee2db7c49d
Anders Balling Petersen <andersballing1991@gmail.com>
2018-02-06 17:34:53 +0100

    Added active class to selected model - styling missing

--- BMHjemmeside/BMHjemmeside/app/controllers/technologyController.js
+++ BMHjemmeside/BMHjemmeside/app/controllers/technologyController.js
@@ -2,14 +2,21 @@
 
     $scope.modelToRender = null;
     $scope.teknologier = null;
+    $scope.showmodels = false;
+    $scope.selectedModel = null;
 
     $http.get('../app/content/maskiner.json')
         .then(function (result) {
             $scope.teknologier = result.data;
         });
 
-    $scope.chosenModel = function (model) {
+    $scope.chosenModel = function (model, index) {
         $scope.modelToRender = model;
         $scope.trustedDescription = $sce.trustAsHtml($scope.modelToRender.description);
+        setActive(index);
     }
+
+    var setActive = function (index) {
+        $scope.selectedModel = index;
+    };
 });
